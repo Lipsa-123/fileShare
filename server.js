bd4f03003a104f9ec47e573b17fc652e03b3cca9
Lipsa Patro <lipsap300@gmail.com>
2022-09-01 15:36:02 +0530

    changed successfully

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs');
 
 
-app.use('api/files', require('./routes/files'))
-app.use('files', require('./routes/show'))
-app.use('files/download', require('./routes/download'))
+app.use('/api/files', require('./routes/files'))
+app.use('/files', require('./routes/show'))
+app.use('/files/download', require('./routes/download'))
 
 app.listen(PORT, ()=>{
     console.log(`Listening on PORT ${PORT}`)
